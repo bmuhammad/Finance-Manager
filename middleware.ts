d7ef7b8f32146456cbf085f160f6ad3dcaae80f8
Brian <briancmuhammad@yahoo.com>
2024-08-02 14:32:41 -0400

    added/ finished navigation bar /details

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher(["/",
+  "/api(.*)"
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {
